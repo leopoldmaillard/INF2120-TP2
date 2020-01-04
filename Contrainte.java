b29473687ce9f98273cfd7fd5e44dba413742884
leopoldmaillard <56095779+leopoldmaillard@users.noreply.github.com>
2020-01-04 17:55:42 -0500

    Update Contrainte.java

--- Contrainte.java
+++ Contrainte.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+/*
+ * @author Lisa Casino
+ * @author Léopold Maillard
+ */
+
 @SuppressWarnings("serial")
 public class Contrainte extends ArrayList< Intervalle > {
    
@@ -11,14 +16,14 @@ public class Contrainte extends ArrayList< Intervalle > {
     }
 
     public Contrainte( Collection< Intervalle > collection ) {
-    	super(collection);
+    	super( collection );
     	this.trierContrainte();
     	this.disjonction();    	
     }
 
 
     public boolean add( Intervalle intervalle ){
-    	super.add(intervalle);
+    	super.add( intervalle );
     	this.trierContrainte();
     	this.disjonction();
     	
@@ -30,48 +35,48 @@ public class Contrainte extends ArrayList< Intervalle > {
     	int i = 0;
 
     	int taille = this.size();
-    	while( i < taille) {
+    	while( i < taille ) {
     		 
     		// borne inferieure se trouve à gauche de l'intervalle i
     		// et la borne superieure traverse l'intervalle i 
     		// 		deux cas : borne superieure dans l'intervale i
     		//                 borne superieure à droite de l'intervalle i
-    		if ((this.get(i).debut >= intervalle.debut ) &&
-    				(intervalle.fin >= this.get(i).debut)){
+    		if ( ( this.get(i).debut >= intervalle.debut ) &&
+    				( intervalle.fin >= this.get(i).debut ) ){
     			
     			// borne superieure se trouve à droite de l'intervalle i
     			// on supprime totalement l'intervalle i
-    			if (intervalle.fin >= this.get(i).fin) {
-    				super.remove(i);
+    			if ( intervalle.fin >= this.get(i).fin ) {
+    				super.remove( i );
     				taille--;
-    				i--; //LEO
+    				i--;
     				
     			// borne superieure se trouver dans l'intervalle i 
     			// on supprime une partie de l'intervalle i 
     			} else {
-    				super.add(i, new Intervalle(intervalle.fin, this.get(i).fin));
-    				super.remove(i+1);
+    				super.add( i , new Intervalle( intervalle.fin , this.get(i).fin ) );
+    				super.remove( i+1 );
     			}
     			
     		// borne inférieure se trouve au milieu de l'intervalle i
-    		} else if ((this.get(i).debut < intervalle.debut ) &&
-    				(intervalle.debut < this.get(i).fin)) {
+    		} else if ( ( this.get(i).debut < intervalle.debut ) &&
+    				( intervalle.debut < this.get(i).fin ) ) {
     			
     			// borne superieure se trouve dans l'intervalle
     			// l'intervalle se coupe en deux
-    			if (intervalle.fin < this.get(i).fin) {
-    				Intervalle inter1 = new Intervalle(this.get(i).debut, intervalle.debut);
-    				Intervalle inter2 = new Intervalle(intervalle.fin, this.get(i).fin);
-    				super.add(i, inter1);
-    				super.add(i+1, inter2);
-    				super.remove(i+2);
+    			if ( intervalle.fin < this.get(i).fin ) {
+    				Intervalle inter1 = new Intervalle( this.get(i).debut, intervalle.debut );
+    				Intervalle inter2 = new Intervalle( intervalle.fin , this.get(i).fin );
+    				super.add( i , inter1 );
+    				super.add( i+1 , inter2 );
+    				super.remove( i+2 );
     				taille++;
     			
     			// borne superieure se trouver au-dela de l'intervalle i
     			// on ne garde que le début de l'intervalle i
     			} else {
-    				super.add(i, new Intervalle(this.get(i).debut, intervalle.debut));
-    				super.remove(i+1);
+    				super.add( i , new Intervalle( this.get(i).debut , intervalle.debut ) );
+    				super.remove( i+1 );
     			}		
     		}	
     		i += 1;
@@ -79,10 +84,11 @@ public class Contrainte extends ArrayList< Intervalle > {
     }
 
 
-    public boolean appartient( int v ){     // algo indeterministe normalement
+    public boolean appartient( int v ){   
     	boolean resultat = false;
     	
-    	for( int i = 0 ; i < this.size(); i++ ) {
+    	//On regarde si v appartient à un des intervalles de la contrainte
+    	for( int i = 0 ; i < this.size(); i++ ) {	
     		if ( v >= this.get(i).debut && v < this.get(i).fin ) {
     			resultat = true ;
     		}
@@ -95,7 +101,7 @@ public class Contrainte extends ArrayList< Intervalle > {
     	int i = 0;
     	int compteurTotal = 0;
     	int compteur = 0;
-    	while ( i < this.size()) {
+    	while ( i < this.size() ) {
     		compteur = this.get(i).fin - this.get(i).debut;
     		compteurTotal = compteur + compteurTotal;
     		i++;
@@ -106,8 +112,11 @@ public class Contrainte extends ArrayList< Intervalle > {
 
     public Contrainte union( Contrainte contrainte ){
     	Contrainte resultat = this ;
-    	for (int i = 0 ; i < contrainte.size() ; i++) {
-    		resultat.add(contrainte.get(i));
+    	
+    	//On ajoute au resultat chaque intervalle de la contrainte en paramètre
+    	//à l'aide de la méthode add
+    	for ( int i = 0 ; i < contrainte.size() ; i++ ) {
+    		resultat.add( contrainte.get(i) );
     	}
     	return resultat ;
     }
@@ -117,11 +126,13 @@ public class Contrainte extends ArrayList< Intervalle > {
      	
     	Contrainte res = new Contrainte();
     	
-    	for (int i = 0 ; i < this.size(); i++) {
-    		for (int j = 0 ; j < contrainte.size() ; j++) {
-    			
-    			if (!(this.get(i).intersection(contrainte.get(j)).isEmpty())){
-    			res.add(this.get(i).intersection(contrainte.get(j)).get(0));
+    	//On regarde les intersections des intervalles des deux contraines 2 à 2
+    	for ( int i = 0 ; i < this.size() ; i++ ) {
+    		for ( int j = 0 ; j < contrainte.size() ; j++ ) {
+    			//S'il existe une intersection, on l'ajoute au résultat
+    			//avec la méthode add qui respecte les invariants d'une contrainte
+    			if ( !( this.get(i).intersection( contrainte.get(j) ).isEmpty() ) ){
+    				res.add( this.get(i).intersection( contrainte.get(j) ).get(0) );
     			}
     		}
     	}    	
@@ -130,8 +141,8 @@ public class Contrainte extends ArrayList< Intervalle > {
 
     public Contrainte soustraction( Contrainte contrainte ){
     	Contrainte resultat = this ;
-    	for (int i = 0 ; i < contrainte.size() ; i++) {
-    		resultat.remove(contrainte.get(i));
+    	for ( int i = 0 ; i < contrainte.size() ; i++ ) {
+    		resultat.remove( contrainte.get(i) );
     	}
     	return resultat ;
     }
@@ -144,52 +155,58 @@ public class Contrainte extends ArrayList< Intervalle > {
     	}
     	return str ;
     }
-    
-    //Classes supplémentaires
-    
-    //Tri des intervalles d'une contrainte (par borne supérieure croissante)
+        
+    /*
+     * Trie les intervalles de la contrainte dans l'ordre des bornes supérieures croissantes.
+     * Permet ainsi de respecter les invariants d'une contrainte (éléments de la liste en ordre croissant).
+     */
     public void trierContrainte() {
     	
+    	//tri à bulle
     	boolean enOrdre ;
     	Intervalle temp ;
     	do {
     		enOrdre = true ;
-    		for ( int i = 0 ; i < this.size() - 1 ;  i++) {
-    			if (this.get(i).fin > this.get(i+1).fin) {
+    		for ( int i = 0 ; i < this.size() - 1 ;  i++ ) {
+    			if ( this.get(i).fin > this.get(i+1).fin ) {
     				temp = this.get(i);
-    				this.set(i, this.get(i+1));
-    				this.set(i+1, temp);
+    				this.set( i , this.get(i+1) );
+    				this.set( i+1 , temp );
     				enOrdre = false ;
     			}
     		}
-    	}while ( !enOrdre);	
+    	}while ( !enOrdre );	
     }
     
-    //Va disjoindre les intervalles de la contrainte comme spécifié dans les invariants de la contrainte
-    public void disjonction () {
+    /*
+     * Disjoint les intervalles de la contrainte.
+     * Permet ainsi de respecter les invariants d'une contrainte.
+     * L'intersection entres deux intervalles d'une liste est ainsi un intervalle vide.
+     */
+    public void disjonction() {
     	int taille = this.size() ;
     	
-    	for (int i = 0 ; i < taille ; i++ ) {
-    		for (int j = i + 1 ; j < taille ; j++) {
+    	for ( int i = 0 ; i < taille ; i++ ) {
+    		for ( int j = i + 1 ; j < taille ; j++ ) {
     			
     			//Si l'intervalle suivant est inclu dans l'intervalle courant, on le supprime
-    			if (this.get(i).debut <= this.get(j).debut && this.get(i).fin >= this.get(j).fin) { 
-        			super.remove(j);
+    			if ( this.get(i).debut <= this.get(j).debut && this.get(i).fin >= this.get(j).fin ) { 
+        			super.remove( j );
         			taille--;
         			j = i ;
         		}
         		
     			//Si l'intervalle courant est inclu dans l'intervalle suivant, on le supprime
-        		else if (this.get(i).debut >= this.get(j).debut && this.get(i).fin <= this.get(j).fin) {
-        			super.remove(i);
+        		else if ( this.get(i).debut >= this.get(j).debut && this.get(i).fin <= this.get(j).fin ) {
+        			super.remove( i );
         			taille--;
         			j = i ;
         		}
         		
     			//Disjonction
-        		else if (this.get(i).fin >= this.get(j).debut) {
-        			this.set(i, new Intervalle ( this.get(i).debut ,  this.get(j).fin ));
-        			super.remove(j);
+        		else if (this.get( i ).fin >= this.get( j ).debut) {
+        			this.set( i , new Intervalle ( this.get(i).debut ,  this.get(j).fin ) );
+        			super.remove( j );
         			taille--;
         			j = i ;
         		}	
